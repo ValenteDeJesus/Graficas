40f3537ae8222aea20738059e5455fdbb5582fc5
ValenteDeJesus <68664878+ValenteDeJesus@users.noreply.github.com>
2020-08-12 22:42:06 -0500

    Mapas divididos con geojsons en leaflet

---
+++ maps/map.js
@@ -0,0 +1,93 @@
+const tilesProvider = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+
+let map = L.map('map').setView([20.51,-98.82],8)
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 18,
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
+
+
+//let marker = L.marker([20.12,-98.75]).addTo(map)
+/*code to create custom marker
+let iconMarker = L.icon({
+	iconUrl: 'marker.png',
+	iconSize: [60,60],
+	iconAnchor: [30,60]
+
+})
+
+let marker2 = L.marker([20.15,-98.78], {icon: iconMarker}).addTo(map)
+*/
+
+/* IMPORTANT features forcustom style
+L.geoJSON(states, {
+    style: function(feature) {
+        switch (feature.properties.party) {
+            case 'Republican': return {color: "#ff0000"};
+            case 'Democrat':   return {color: "#0000ff"};
+        }
+    }
+}).addTo(map);
+
+
+fetch(
+	"json/secciones.geojson"
+	).then(
+	res => res.json()
+	).then(
+	data => {
+		orig = L.geoJSON(data, {
+    	color: "#000000"
+        }
+	).addTo(map)
+	}
+	)
+
+*/
+//L.geoJSON(states).addTo(map);
+
+changeMap("json/entidad.geojson")
+
+map.on('dblclick', e => {
+	let latLng = map.mouseEventToLatLng(e.originalEvent)
+	console.log(latLng)
+})
+
+
+
+
+document.getElementById('select-division').addEventListener('change', function(e){
+  map.removeLayer(orig)
+  let op = e.target.value;
+  switch (op) {
+            case "0": changeMap("json/entidad.geojson"); break;
+            case "1": changeMap("json/municipios.geojson"); break;
+            case "2": changeMap("json/distritos_federales.geojson");break;
+            case "3": changeMap("json/distritos_locales.geojson");break;
+            case "4": changeMap("json/secciones.geojson");break;
+
+        }
+});
+
+
+
+function changeMap(route){
+	//window.polygon.remove()
+	fetch(
+	route
+	).then(
+	res => res.json()
+	).then(
+	data => {
+		orig = L.geoJSON(data, {
+    	color: "#000000"
+        }
+	).addTo(map)
+	}
+	)
+}
+
